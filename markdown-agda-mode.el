34d6d114deb260be57ae3dd7d404c2ea3ee79a74
Alex Rice <alexrice999@hotmail.co.uk>
2020-05-26 15:18:42 +0100

    Initial commit

---
+++ markdown-agda-mode.el
@@ -0,0 +1,50 @@
+;;; markdown-agda-mode.el --- Major mode for working with literate markdown agda files
+;;; -*- lexical-binding: t
+
+;;; Commentary:
+
+;; A major mode for editing Agda code embedded in markdown files (.lagda.md files.)
+;; https://agda.readthedocs.io/en/v2.6.1/tools/literate-programming.html#literate-org
+
+;;; Code:
+
+(require 'polymode)
+(require 'agda2-mode)
+
+(defgroup markdown-agda-mode nil
+  "markdown-agda-mode customisations"
+  :group 'languages)
+
+(defcustom use-agda-input t
+  "Whether to use Agda input mode in non-Agda parts of the file."
+  :group 'org-agda-mode
+  :type 'boolean)
+
+(define-hostmode poly-markdown-agda-hostmode
+  :mode 'markdown-mode
+  :keep-in-mode 'host)
+
+(define-innermode poly-org-agda-innermode
+  :mode 'agda2-mode
+  :head-matcher "```agda"
+  :tail-matcher "```"
+  ;; Keep the code block wrappers in markdown mode, so they can be folded, etc.
+  :head-mode 'markdown-mode
+  :tail-mode 'markdown-mode
+  ;; Disable font-lock-mode, which interferes with Agda annotations,
+  ;; and undo the change to indent-line-function Polymode makes.
+  :init-functions '((lambda (_) (font-lock-mode 0))
+                    (lambda (_) (setq indent-line-function #'indent-relative))))
+
+(define-polymode org-agda-mode
+  :hostmode 'poly-markdown-agda-hostmode
+  :innermodes '(poly-markdown-agda-innermode)
+  (when use-agda-input (set-input-method "Agda")))
+
+(assq-delete-all 'background agda2-highlight-faces)
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.lagda.md" . markdown-agda-mode))
+
+(provide 'org-agda-mode)
+;;; markdown-agda-mode ends here
